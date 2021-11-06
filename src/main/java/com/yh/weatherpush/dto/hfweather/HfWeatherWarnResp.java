bf88bfd752cac6e6b068c93dfc06dc67702187ce
yanghao <yangh@peoplecommnue.com>
2021-11-06 10:38:23 +0800

    添加明日天气预报

---
+++ src/main/java/com/yh/weatherpush/dto/hfweather/HfWeatherWarnResp.java
@@ -0,0 +1,17 @@
+package com.yh.weatherpush.dto.hfweather;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author : yh
+ * @date : 2021/11/5 22:39
+ */
+@Data
+public class HfWeatherWarnResp implements Serializable {
+
+    private static final long serialVersionUID = -4052389515435736746L;
+
+
+}
---
+++ src/main/java/com/yh/weatherpush/dto/hfweather/WeatherWarn.java
@@ -0,0 +1,12 @@
+package com.yh.weatherpush.dto.hfweather;
+
+import java.io.Serializable;
+
+/**
+ * @author : yh
+ * @date : 2021/11/5 22:40
+ */
+public class WeatherWarn implements Serializable {
+
+
+}
