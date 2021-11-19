b12c5cc4ea084a9816f3c676d2a3470651d1691d
yanghao <yangh@peoplecommnue.com>
2021-11-19 20:55:14 +0800

    服务器部署脚本

---
+++ src/main/resources/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# $1 新镜像id或名字
+if [ -z "$1" ];then
+    echo "请输入新镜像id或名字！"
+    exit 0
+fi
+old_container=`docker ps -q --filter "name=weather-push" --format "{{.ID}}" | awk '{print $1}'`
+echo "old_container=" $old_container
+old_image=`docker ps -q --filter "name=weather-push" --format "{{.Image}}" | awk '{print $1}'`
+echo "old_image=" $old_image
+docker stop $old_container
+docker rm $old_container
+docker rmi $old_image
+new_container=`docker run -d -p 6666:6666 --name="weather-push" $1`
+echo "new_container=" $new_container
