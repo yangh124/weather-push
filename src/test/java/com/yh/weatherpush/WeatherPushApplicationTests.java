0ad3f716c3837aa979b1d5f1d9a4e4eb22c19a2f
yanghao <yh.124@qq.com>
2022-05-03 18:52:32 +0800

    修改readme

--- src/test/java/com/yh/weatherpush/WeatherPushApplicationTests.java
+++ src/test/java/com/yh/weatherpush/WeatherPushApplicationTests.java
@@ -1,26 +1,31 @@
 package com.yh.weatherpush;
 
-import com.yh.weatherpush.entity.Tag;
-import com.yh.weatherpush.service.RedisService;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class WeatherPushApplicationTests {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private RedissonClient redissonClient;
 
     @Test
     void contextLoads() {
-        String encode = passwordEncoder.encode("123456");
-        System.out.println(encode);
+        RLock lock = redissonClient.getLock("test");
+        lock.lock();
+        System.out.println("上锁");
+        try {
+            TimeUnit.MINUTES.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
 }
