a1e6a3b4dfd9137915e4e80cf5a464920554c8ed
yanghao <yh.124@qq.com>
2022-05-11 22:48:41 +0800

    修改配置文件

--- src/test/java/com/yh/weatherpush/WeatherPushApplicationTests.java
+++ src/test/java/com/yh/weatherpush/WeatherPushApplicationTests.java
@@ -1,31 +1,15 @@
 package com.yh.weatherpush;
 
 import org.junit.jupiter.api.Test;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class WeatherPushApplicationTests {
 
-    @Autowired
-    private RedissonClient redissonClient;
-
     @Test
     void contextLoads() {
-        RLock lock = redissonClient.getLock("test");
-        lock.lock();
-        System.out.println("上锁");
-        try {
-            TimeUnit.MINUTES.sleep(1);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            lock.unlock();
-        }
+
     }
 
 }
