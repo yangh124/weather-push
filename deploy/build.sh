0b0c4a41aced970981055dfc2440235ee20edd61
yanghao <yh.124@qq.com>
2022-06-26 13:11:26 +0800

    修改build脚本

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-jar_file_name=`find ../target -name "*.jar" | awk -F/ '{print $3}'`
-echo $jar_file_name
-if [ -z "$1" ];then
-    echo "请输入版本号！"
-    exit 0
-fi
-docker build -t registry.cn-hangzhou.aliyuncs.com/yh123/weather-push:$1 --build-arg JAR_FILE=$jar_file_name ../
+VERSION=`find ../target -name "*.jar" | awk -F- '{print $3 "-" $4}' | awk -F.jar '{print $1}'`
+echo '当前build版本：'$VERSION
+JAR_FILE_NAME=weather-push-$VERSION.jar
+TAG=registry.cn-hangzhou.aliyuncs.com/yh123/weather-push:$VERSION
+docker build -t $TAG --build-arg JAR_FILE=$JAR_FILE_NAME ../
