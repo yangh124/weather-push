ac23d1e32222bf8c33591afb45d0876a250aa9e1
yanghao <yh.124@qq.com>
2022-08-03 20:54:24 +0800

    修改build脚本

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # $1 新镜像id或名字
+NAME=weather-push
 if [ -z "$1" ];then
     echo "请输入新镜像id或名字！"
     exit 0
 fi
-old_container=`docker ps -q --filter "name=weather-push" --format "{{.ID}}" | awk '{print $1}'`
+old_container=`docker ps -q --filter "name=$NAME" --format "{{.ID}}" | awk '{print $1}'`
 echo "old_container=" $old_container
-old_image=`docker ps -q --filter "name=weather-push" --format "{{.Image}}" | awk '{print $1}'`
+old_image=`docker ps -q --filter "name=$NAME" --format "{{.Image}}" | awk '{print $1}'`
 echo "old_image=" $old_image
 docker stop $old_container
 docker rm $old_container
 docker rmi $old_image
-new_container=`docker run -d -p 8080:8080 --name="weather-push" $1`
+new_container=`docker run -d -p 8080:8080 --name="$NAME" $1`
 echo "new_container=" $new_container
