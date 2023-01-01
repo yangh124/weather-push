48bb5cd5839f1097ef30f6ca131e0d6a3cfa96b5
yangh <yh.124@qq.com>
2023-01-01 21:35:25 +0800

    修改缓存分隔符为冒号

--- src/main/java/com/yh/weatherpush/config/redis/CacheConfig.java
+++ src/main/java/com/yh/weatherpush/config/redis/CacheConfig.java
@@ -1,20 +1,17 @@
 package com.yh.weatherpush.config.redis;
 
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 
 /**
- * @author WGR
- * @create 2020/6/28 -- 0:29
+ * @author yangh
  */
 @EnableCaching
 @Configuration
@@ -24,17 +21,16 @@ public class CacheConfig {
     RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
         config = config
-            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
         config = config.serializeValuesWith(
-            RedisSerializationContext.SerializationPair.fromSerializer(new GenericFastJsonRedisSerializer()));
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericFastJsonRedisSerializer()));
 
         CacheProperties.Redis redisProperties = cacheProperties.getRedis();
         if (redisProperties.getTimeToLive() != null) {
             config = config.entryTtl(redisProperties.getTimeToLive());
         }
         if (redisProperties.getKeyPrefix() != null) {
-            CacheKeyPrefix cacheKeyPrefix = CacheKeyPrefix.prefixed(redisProperties.getKeyPrefix());
-            config = config.computePrefixWith(cacheKeyPrefix);
+            config = config.computePrefixWith(name -> redisProperties.getKeyPrefix() + name + ":");
         }
         if (!redisProperties.isCacheNullValues()) {
             config = config.disableCachingNullValues();
