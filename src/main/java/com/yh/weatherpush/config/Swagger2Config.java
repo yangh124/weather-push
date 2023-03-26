4a4fe39a345c0d42e9b30f784edb24d7ae494a0c
yangh124 <yh.124@qq.com>
2023-03-26 15:04:38 +0800

    修改swagger

--- src/main/java/com/yh/weatherpush/config/Swagger2Config.java
+++ src/main/java/com/yh/weatherpush/config/Swagger2Config.java
@@ -7,13 +7,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger2API文档的配置
- * 
+ *
  * @author yh
  */
 @Configuration
@@ -26,16 +27,17 @@ public class Swagger2Config {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).enable(enabled).select()
-            // 为当前包下controller生成API文档
-            .apis(RequestHandlerSelectors.basePackage("com.yh.weatherpush.controller"))
-            // 为有@Api注解的Controller生成API文档
-            // .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-            // 为有@ApiOperation注解的方法生成API文档
-            // .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-            .paths(PathSelectors.any()).build();
+                // 为当前包下controller生成API文档
+                .apis(RequestHandlerSelectors.basePackage("com.yh.weatherpush.controller"))
+                // 为有@Api注解的Controller生成API文档
+                // .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                // 为有@ApiOperation注解的方法生成API文档
+                // .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any()).build();
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("天气推送").description("天气推送").contact("yanghao").version("1.0").build();
+        Contact contact = new Contact("yanghao", "https://github.com/yangh124", "yh.124@qq.com");
+        return new ApiInfoBuilder().title("天气推送").description("天气推送").contact(contact).version("1.0").build();
     }
 }
