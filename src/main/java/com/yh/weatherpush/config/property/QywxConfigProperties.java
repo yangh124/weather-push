18125d68bd02bfc0688f677984c5f73ebb06fe58
yangh <yh.124@qq.com>
2023-04-27 21:48:41 +0800

    删除配置

--- src/main/java/com/yh/weatherpush/config/property/QywxConfigProperties.java
+++ src/main/java/com/yh/weatherpush/config/property/QywxConfigProperties.java
@@ -3,10 +3,7 @@ package com.yh.weatherpush.config.property;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.stereotype.Component;
-
-import java.io.Serializable;
 
 /**
  * @author : yh
@@ -24,57 +21,5 @@ public class QywxConfigProperties {
 
     private String otherSecret;
 
-    private String tokenUrl;
-
-    private String sendUrl;
-
     private String agentid;
-
-    private String baseUrl;
-
-    @NestedConfigurationProperty
-    private TagConfig tag;
-
-    @NestedConfigurationProperty
-    private MemberConfig member;
-
-    @Getter
-    @Setter
-    public static class TagConfig implements Serializable {
-
-        private static final long serialVersionUID = 6594782376511318128L;
-
-        private String createUrl;
-
-        private String listUrl;
-
-        private String deleteUrl;
-
-        private String addTagUserUrl;
-
-        private String delTagUserUrl;
-    }
-
-    @Getter
-    @Setter
-    public static class MemberConfig implements Serializable {
-
-        private static final long serialVersionUID = -7283609725180969658L;
-
-        /**
-         * 邀请加入二维码
-         */
-        private String joinQrcodeUrl;
-
-        /**
-         * 获取部门成员
-         */
-        private String simpleListUrl;
-
-        /**
-         * 获取标签成员
-         */
-        private String tagMemberList;
-
-    }
 }
