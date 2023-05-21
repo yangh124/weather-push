89c4456624a74256198a954e0ab2261f26d5275a
yangh <yh.124@qq.com>
2023-05-21 20:03:02 +0800

    企业微信通讯录组件

--- src/main/java/com/yh/weatherpush/controller/MemberController.java
+++ src/main/java/com/yh/weatherpush/controller/MemberController.java
@@ -1,14 +1,19 @@
 package com.yh.weatherpush.controller;
 
 import com.yh.weatherpush.dto.Result;
+import com.yh.weatherpush.dto.qywx.MemberDTO;
 import com.yh.weatherpush.dto.qywx.QywxAgentConfigDTO;
 import com.yh.weatherpush.manager.QywxManager;
+import com.yh.weatherpush.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author : yh
@@ -21,12 +26,21 @@ public class MemberController {
 
     @Autowired
     private QywxManager qywxManager;
+    @Autowired
+    private MemberService memberService;
 
     @ApiOperation("获取加入企业二维码")
     @GetMapping("/qr_code")
     public Result<String> getJoinQrCode() {
         String joinQrCode = qywxManager.getJoinQrCode();
         return Result.success(joinQrCode);
+    }
+
+    @ApiOperation("获取地区成员/所有成员")
+    @GetMapping()
+    public Result<List<MemberDTO>> memberList(@RequestParam(required = false) Integer id) {
+        List<MemberDTO> memberResps = memberService.memberList(id);
+        return Result.success(memberResps);
     }
 
     @ApiOperation("获取企业微信配置（用于通讯录展示）")
