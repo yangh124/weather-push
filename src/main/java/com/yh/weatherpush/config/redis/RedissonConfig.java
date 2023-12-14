5d2f64f0de07cbbb590b0692086fb989cd19f1e8
yangh <yh.124@qq.com>
2023-12-14 22:39:09 +0800

    cache-config.yaml

--- src/main/java/com/yh/weatherpush/config/redis/RedissonConfig.java
+++ src/main/java/com/yh/weatherpush/config/redis/RedissonConfig.java
@@ -13,16 +13,12 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
-import org.redisson.spring.cache.CacheConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author : yh
@@ -60,9 +56,6 @@ public class RedissonConfig {
 
     @Bean
     public CacheManager cacheManager(RedissonClient redissonClient) {
-        Map<String, CacheConfig> config = new HashMap<>();
-        // 创建一个名称为"testMap"的缓存，过期时间ttl为24分钟，同时最长空闲时maxIdleTime为12分钟。
-        config.put("admin", new CacheConfig(60 * 60 * 1000, 30 * 60 * 1000));
-        return new RedissonSpringCacheManager(redissonClient, config);
+        return new RedissonSpringCacheManager(redissonClient, "classpath:/cache-config.yaml");
     }
 }
