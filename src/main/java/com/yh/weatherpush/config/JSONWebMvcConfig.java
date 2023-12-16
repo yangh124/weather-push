39490c1ceda00386d63ec13af74b30b0d24a3867
yangh <yh.124@qq.com>
2023-12-16 13:30:04 +0800

    fastjson2

--- src/main/java/com/yh/weatherpush/config/JSONWebMvcConfig.java
+++ src/main/java/com/yh/weatherpush/config/JSONWebMvcConfig.java
@@ -6,10 +6,13 @@ import com.alibaba.fastjson2.support.config.FastJsonConfig;
 import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -44,6 +47,14 @@ public class JSONWebMvcConfig implements WebMvcConfigurer {
 
     @Bean
     public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
-        return new FastJsonHttpMessageConverter();
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        //自定义配置...
+        FastJsonConfig config = new FastJsonConfig();
+        config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+        config.setWriterFeatures(JSONWriter.Feature.WriteMapNullValue, JSONWriter.Feature.PrettyFormat);
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        return converter;
     }
 }
