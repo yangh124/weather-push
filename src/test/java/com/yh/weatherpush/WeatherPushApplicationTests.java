c659ab45e34553bbb270332dfe62a8b6a7c4b2fd
yangh <yh.124@qq.com>
2024-02-10 14:31:28 +0800

    密码使用 md5

--- src/test/java/com/yh/weatherpush/WeatherPushApplicationTests.java
+++ src/test/java/com/yh/weatherpush/WeatherPushApplicationTests.java
@@ -1,9 +1,6 @@
 package com.yh.weatherpush;
 
-import cn.dev33.satoken.secure.SaSecureUtil;
-import com.yh.weatherpush.controller.AdminController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -12,12 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("dev")
 class WeatherPushApplicationTests {
 
-    @Autowired
-    private AdminController adminController;
-
     @Test
     void contextLoads() {
-        String s = SaSecureUtil.md5("As071602q");
-        System.out.println(s);
     }
 }
